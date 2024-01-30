5d6243debd10ac9692b4af3674ac8c9a36b7ea61
Blanca Bigeriego <blancabcbt@gmail.com>
2024-01-30 21:43:50 +0100

    Add routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { SingupComponent } from './components/singup/singup.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
-import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SingupComponent } from "./components/singup/singup.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { RequestResetComponent } from "./components/password/request-reset/request-reset.component";
+import { ResponseResetComponent } from "./components/password/response-reset/response-reset.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { RouterModule, RouterOutlet } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -17,12 +19,10 @@ import { ResponseResetComponent } from './components/password/response-reset/res
     SingupComponent,
     ProfileComponent,
     RequestResetComponent,
-    ResponseResetComponent
+    ResponseResetComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterOutlet, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
